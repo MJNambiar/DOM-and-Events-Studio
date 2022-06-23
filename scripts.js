0b74648d3f2abdc16438f7295cab2e18787c4fe9
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-06-22 20:35:06 -0500

    First Studio Code

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+function init () {
+    
+    const takeOffButton = document.getElementById("takeoff");
+    const shuttleBackground = document.getElementById ("shuttleBackground");
+    const shuttleHeight = document.getElementById("spaceShuttleHeight");
+
+    takeOffButton.addEventListener("click", () => {
+        let result = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (result = true) {
+            paragraph.setAttribute ("flightStatus", "Shuttle in flight.");
+            shuttleBackground.style.backgroundColor = "blue";
+            shuttleHeight = 10000
+
+        }
+
+    })
+}
+
+window.addEventListener("load", init);
